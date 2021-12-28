f01b81192004a7b74ed35b222c549609666bd7a2
Fernando Theirs <fntheirs@gmail.com>
2021-12-28 15:08:20 +0100

    replace token symbol

--- app/src/secret.c
+++ app/src/secret.c
@@ -32,7 +32,7 @@ void secret_accept() {
 
 static char *secret_message =
         "USE AT YOUR OWN RISK!! "
-        "You are about to enable the KSM recovery mode."
+        "You are about to enable the PDEX recovery mode."
         "If you are not sure why you are here, reject or unplug your device immediately."
         "Activating this mode will temporarily allow you to sign transactions using Polkadot keys";
 
